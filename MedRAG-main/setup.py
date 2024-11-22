66a8bf3ca819ce178c5bd1aa2355256bc0236139
JuaniTollo <97892941+JuaniTollo@users.noreply.github.com>
2024-11-22 01:42:28 +0000

    Fix importa packedge problems

--- MedRAG-main/setup.py
+++ MedRAG-main/setup.py
@@ -1,6 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="MedRAG2",
+    name="MedRAG",
     version="0.1",
-    packages=find_packages())
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    package_data={"": ["templates/*.jinja", "data/*.py"]},  # Include template and data files
+    install_requires=[
+        'numpy',  # Example dependency, add others as needed
+        'torch',
+        'transformers'
+    ],
+    entry_points={
+        'console_scripts': [
+            'medrag-cli=src.medrag:main',  # Example if you have a CLI
+        ],
+    },
+)
